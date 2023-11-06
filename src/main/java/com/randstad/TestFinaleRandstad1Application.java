8c4b634d72c1dd86ea174a1efa5b349f39316aac
Vito Debellis <61493419+VitoPoggio@users.noreply.github.com>
2023-11-06 13:03:23 +0100

    file iniziali

---
+++ src/main/java/com/randstad/TestFinaleRandstad1Application.java
@@ -0,0 +1,13 @@
+package com.randstad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFinaleRandstad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFinaleRandstad1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/randstad/TestFinaleRandstad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randstad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFinaleRandstad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
